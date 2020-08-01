6d23f38e19c213c886f00719ed133d3b67af4508
Oscar_Warcave <oscar@warcave.com>
2020-08-02 00:06:09 +0200

    - First commit - Tile system

---
+++ Funimal Fair/Assets/Scripts/DrawConnectionScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(LineRenderer))]
+public class DrawConnectionScript : MonoBehaviour
+{
+    private LineRenderer _lineRenderer = null;
+
+    private void Awake()
+    {
+        UpdateLineSettings();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTileConnection();
+    }
+
+    public void UpdateTileConnection()
+    {
+        _lineRenderer.SetPosition(0, new Vector3(transform.position.x, 0, transform.position.z));
+        if (this.GetComponent<BaseTileScript>().NextTile!=null)
+        {
+            Vector3 nexttilePos = this.GetComponent<BaseTileScript>().NextTile.transform.position;
+            _lineRenderer.SetPosition(1, new Vector3(nexttilePos.x, 0, nexttilePos.z));
+        }
+        UpdateLineSettings();
+    }
+
+    private void UpdateLineSettings()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.alignment = LineAlignment.TransformZ;
+        _lineRenderer.widthMultiplier = 0.2f;
+    }
+}
---
+++ Funimal Fair/Assets/Scripts/EDITOR DrawConnectionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DrawConnectionScript))]
+public class EDITORDrawConnectionScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        DrawConnectionScript _drawConnectionScript = (DrawConnectionScript)target;
+        if (GUILayout.Button("Update Lines"))
+        {
+
+            foreach (DrawConnectionScript dcs in GameObject.FindObjectsOfType<DrawConnectionScript>())
+            {
+                dcs.UpdateTileConnection();
+            }
+        }
+    }
+}
