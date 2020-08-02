df6099aa41687bdef8bef030d4abce8333548e4e
Oscar_Warcave <oscar@warcave.com>
2020-08-02 18:38:23 +0200

    - Added Tweaning library - Set up basic movement code

---
+++ Funimal Fair/Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerPrefab;
+    private GameObject _startTile;
+    private GameObject _player;
+
+    private void Awake()
+    {
+        FindStartTile();
+        InstantiatePlayer();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FindStartTile()
+    {
+        _startTile = GameObject.FindObjectOfType<StartTileScript>().gameObject;
+    }
+
+    private void InstantiatePlayer()
+    {
+        _player = Instantiate(_playerPrefab, _startTile.transform.position, Quaternion.identity);
+        _player.GetComponent<PlayerControllerScript>().SetCurrentTile(_startTile.GetComponent<BaseTileScript>());
+    }
+}
---
+++ Funimal Fair/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+    private BaseTileScript _currentTile = null;
+    private List<int> _ongoingTweens = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageMovePlayer();
+    }
+
+    private void MoveToNextTile()
+    {
+        CancelAllTweensInList(_ongoingTweens);
+        _ongoingTweens.Add(LeanTween.move(gameObject, _currentTile.NextTile.transform.position, _movementSpeed).setEaseInOutSine().id);
+    }
+
+    public void SetCurrentTile(BaseTileScript tile)
+    {
+        _currentTile = tile;
+    }
+
+    private void ManageMovePlayer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToNextTile();
+            SetCurrentTile(_currentTile.NextTile);
+        }
+    }
+
+    private void CancelAllTweensInList(List<int> _tweenListToCancel)
+    {
+        foreach (int tween in _tweenListToCancel)
+        {
+            LeanTween.cancel(tween);
+        }
+    }
+}
