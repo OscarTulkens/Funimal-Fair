35ba5e9640378edc2827cb8e61cd2ec8848fc3d7
OscarTulkens <43540793+OscarTulkens@users.noreply.github.com>
2020-12-24 16:29:27 +0100

    - Added font - Started on Currency UI - Added Floor Texture

---
+++ Funimal Fair/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerScript : MonoBehaviour
+{
+    private int _previousCurrency = 0;
+    private List<int> _tweenList = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ChangeCurrency(int newCurrency)
+    {
+
+    }
+
+
+
+}
